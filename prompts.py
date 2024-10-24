09418539a18a79c73c1d37cdff9a08d3af366bf1
breadbread1984 <breadbread1984@163.com>
2024-10-24 14:59:52 +0800

    update

---
+++ prompts.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def agent_template(tokenizer, tools):
+  prompt = hub.pull('hwchase17/react-json')
+  system_template = prompt[0].prompt.template
+  system_template = system_template.replace('{tools}', render_text_description(tools))
+  system_template = system_template.replace('{tool_names}', ", ".join([t.name for t in tools]))
+  user_template = prompt[1].prompt.template
+  messages = [
+    {'role': 'system', 'content': system_template},
+    {'role': 'user', 'content': user_template}
+  ]
+  prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generation_prompt = True)
+  template = PromptTemplate(template = prompt, input_variables = ['agent_scratchpad', 'input'])
+  return template
