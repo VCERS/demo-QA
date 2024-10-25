d14b90b6ccc24f638457fc5eb09b685b3f0e967d
breadbread1984 <breadbread1984@163.com>
2024-10-25 09:32:37 +0800

    update

--- agent.py
+++ agent.py
@@ -7,7 +7,7 @@ from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.agents.output_parsers import ReActJsonSingleInputOutputParser
 from models import Llama3, Qwen2
 from prompts import agent_template
-from tools import load_precursor_predictor
+from tools import load_precursor_predictor, load_ox_potential_predictor, load_synthesis_steps_predictor
 import config
 
 class Agent(object):
@@ -18,7 +18,9 @@ class Agent(object):
     }
     tokenizer, llm = llms_types[model](True)
     tools = load_tools(tools, llm = llm, serper_api_key = 'd075ad1b698043747f232ec1f00f18ee0e7e8663') + \
-            [load_precursor_predictor(),]
+            [load_precursor_predictor(),
+             load_ox_potential_predictor(),
+             load_synthesis_steps_predictor(tokenizer, llm)]
     prompt = agent_template(tokenizer, tools)
     llm = llm.bind(stop = ["<|eot_id|>"])
     chain = {"input": lambda x: x["input"], "agent_scratchpad": lambda x: format_log_to_str(x["intermediate_steps"])} | prompt | llm | ReActJsonSingleInputOutputParser()
