4ca3f716ddae3c941175e235681aaf31effb0362
breadbread1984 <breadbread1984@163.com>
2024-10-25 10:07:42 +0800

    update

--- main.py
+++ main.py
@@ -17,20 +17,21 @@ def main(unused_argv):
     answer = agent.query(question)
     history.append((question, answer['output']))
     return "", history
-  block = gr.Blocks()
-  with block as demo:
+  with gr.Blocks() as demo:
+    if "history" not in gr.SessionState:
+      gr.SessionState['history'] = []
     with gr.Row(equal_height = True):
       with gr.Column(scale = 15):
         gr.Markdown("<h1><center>LLM Agent</center></h1>")
     with gr.Row():
       with gr.Column(scale = 4):
         chatbot = gr.Chatbot(height = 450, show_copy_button = True)
-        msg = gr.Textbox(label = '需要问什么？')
+        user_input = gr.Textbox(label = '需要问什么？')
         with gr.Row():
           submit_btn = gr.Button("发送")
         with gr.Row():
           clear_btn = gr.ClearButton(components = [chatbot], value = "清空问题")
-      submit_btn.click(query, inputs = [msg, chatbot], outputs = [msg, chatbot])
+      submit_btn.click(query, inputs = [user_input, gr.SessionState['history']], outputs = [chatbot, gr.SessionState['history']])
   gr.close_all()
   demo.launch(server_name = config.service_host, server_port = config.service_port)
 
