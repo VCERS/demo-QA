94c921812e2be8ee3de75c6f44f9fdb2babe9c37
breadbread1984 <breadbread1984@163.com>
2024-10-25 09:29:11 +0800

    update

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,3 +2,4 @@
 
 from .precursor_predictor import load_precursor_predictor
 from .ox_potential_predictor import load_ox_potential_predictor
+from .synthesis_preidctor import load_synthesis_steps_predictor
---
+++ tools/synthesis_predictor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from pydantic import BaseModel, Field
+from typing import Optional, Type, List, Dict, Union, Any
+from langchain.tools import BaseTool, StructuredTool, Tool, tool
+from langchain.callbacks.manager import AsyncCallbackManagerForToolRun, CallbackManagerForToolRun
+from .electrolyte_synthesis import SynthesisSteps
+
+def load_synthesis_steps_predictor(tokenizer, llm):
+  class SynthesisProcedure(BaseModel):
+    steps: List[str] = Field(description = "a list of steps of one synthesis procedure")
+
+  class ExampleOutput(BaseModel):
+    synthesis_procedures: List[SynthesisProcedure] = Field(description = "a list of synthesis procedures")
+
+  class SynthesisStepsPredictorInput(BaseModel):
+    query: str = Field(description = "chemical expression of a compound")
+    n: int = Field(description = "how many procedures are returned. if not specified just use value 1.")
+
+  class SynthesisStepsPredictorConfig(BaseModel):
+    class Config:
+      arbitrary_types_allowed = True
+    predictor: SynthesisSteps
+
+  class SynthesisStepsPredictorTool(StructuredTool):
+    name: str = "electrolyte synthesis procedure predictor"
+    description: str = "predict multiple possible procedures of an electrolyte synthesis"
+    args_schema: Type[BaseModel] = SynthesisStepsPredictorInput
+    config: SynthesisStepsPredictorConfig
+    def _run(self, query: str, n: int, run_manager: Optional[CallbackManagerForToolRun] = None) -> ExampleOutput:
+      procedures = self.config.predictor.predict(query, n)
+      return ExampleOutput(synthesis_procedures = procedures)
+
+  predictor = SynthesisSteps(tokenizer, llm)
+  return SynthesisStepsPredictorTool(config = SynthesisStepsPredictorConfig(
+    predictor = predictor
+  ))
+
+if __name__ == "__main__":
+  tools = load_synthesis_steps_predictor(tokenizer, llm)
+  res = tool.invoke({'query': 'Li6PS5Cl', 'n': 2})
+  print(res)
