08cbdcf790fc45fefb1672f776061da7049205ba
Konquer your Mind <breadbread1984@163.com>
2024-10-25 15:13:10 +0800

    Update synthesis_steps.py

--- tools/electrolyte_synthesis/synthesis_steps.py
+++ tools/electrolyte_synthesis/synthesis_steps.py
@@ -14,7 +14,7 @@ class SynthesisSteps(object):
     results = list()
     for idx, precursors in enumerate(precursors_predicts):
       steps = self.chain.invoke({'precursors': ','.join(precursors), 'target': query})
-      results.append(steps)
+      results.append(steps['steps'])
     return results
 
 
